9b70ced750f7c298ccc9dde1f34d316fe3e3c74a
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-19 08:57:31 +0530

    basic of vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+import Chai from "./chai"
+function App() {
+  return (
+    <Chai/>
+   //<h1>CHAI AUR REACT | ABHAY DWIVEDI</h1>
+  )
+}
+
+export default App
---
+++ vite-project/src/chai.jsx
@@ -0,0 +1,11 @@
+function Chai(){
+    return(
+      <>
+       <h3>chai is ready</h3>
+       <h3>ABHAY boy</h3>
+      </>
+     
+    )
+}
+
+export default Chai
